f04406e9718aefc9b8b65877aeb38883fd748383
INTERASMP <wanchriz42@gmail.com>
2024-08-22 19:04:35 +0700

    Update

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const navbar = document.querySelector('.navbar');
+const navbar = document.querySelector('nav');
 
 window.addEventListener('scroll', () => {
-  if (window.scrollY > 100) {
-    navbar.classList.add('bg-blue-700');
-  } else if (window.scrollY <= 100) {
-    navbar.classList.remove('bg-blue-700');
-  }
-});
+    if (window.scrollY > 500) {
+        navbar.classList.add('bg-blue-700')
+    } else if (window.scrollY <= 600) {
+        navbar.classList.remove('bg-blue-700')
+    }
+})
