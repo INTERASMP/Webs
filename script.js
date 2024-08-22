3a15a0689a8dafddd606633c123785c915a462ff
INTERASMP <wanchriz42@gmail.com>
2024-08-22 07:21:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const navbar = document.querySelector('.navbar');
 
 window.addEventListener('scroll', () => {
   if (window.scrollY > 100) {
-    navbar.classList.add('bg-blue-700');
+    navbar.classList.add('navbar-solid');
   } else if (window.scrollY <= 100) {
-    navbar.classList.remove('bg-blue-700');
+    navbar.classList.remove('navbar-solid');
   }
 });
